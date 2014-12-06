755629e1b3932c5026013c0ee4769e5cb4c1a612
JeffL <jeffreynlaw@gmail.com>
2014-12-06 07:00:39 -0700

    Updated the start_Push.sh script to python

--- Push/push_Data.sh
+++ Push/push_Data.sh
@@ -7,6 +7,7 @@
 #$ -N Push_Data
 #$ -o push_results.log
 #$ -e push_results.log
+#$ -q all.q
 #$ -V
 
 
@@ -18,16 +19,17 @@
 function usage {
 cat << EOF
 USAGE: bash push_Data.sh 
-All of the std_out is written to a csv by start_Push.sh. all std_err is written to the screen.
 	-h | --help
 	-u | --user_server <ionadmin@ip_address>
-	-p | --project_path <path/to/project>	 (Destination on the other server of the files to push.)
-	-s | --sample <sample_name>		(The sample's name)
+	-p | --dest_path <path/to/project>	 (Destination folder or path on the other server of the files to push.)
 	-i | --run_id <run_id> 			(The run's id. Will be used to find the file)
-	-r | --run <run> 			(Files will be pushed to project_path/sample/run)
-	-P | --proton_name <proton_name> 	(i.e. PLU or MER or NEP. Will be used to find the location of the bam file and such)
+	-r | --run_json <Json_Files/Run1.json> 			(The run's json file to push)
+	-s | --sample_json </projects/Eintsein/E001/E001.json> 			(The sample's json file on the server. if it already exists, a run will be added to it. If not, it will be pushed)
+	-P | --proton_name <proton_name> 	(i.e. PLU or MER or NEP or ROT. Will be used to find the location of the bam file and such)
 	-bp | --backup_path <backup_path> 	(for pluto: /mnt/Charon/archivedReports 	for mercury: /mnt/Triton/archivedReports)
 	-b | --barcode <barcode>		(if this run is barcoded)
+	-o | --output_csv <push_results.csv>	(append the results of this samples copy to the file)
+	-l | --log_file <push.log>		(Default: push.log) 
 EOF
 exit 8
 }
@@ -48,7 +50,7 @@ function check_TS_Version {
 			TS_version="not_found"
 		fi
 	fi
-	printf "${TS_version},"	
+	OUTPUT="${OUTPUT}${TS_version},"	
 }
 #find the version and parameters TVC was run on the original BAM file.
 # $1: The Run_Path/plugin_out/variantCaller_out/drmaa_stdout.txt
@@ -58,10 +60,10 @@ function check_TVC_Version {
 			grep -oe "[0-9]\.[0-9]" | perl -ne "chomp and print"`
 		TVC_parameters=`grep -oiE "results[/_.a-z]+\.json" $1 2>/dev/null | \
 			grep -oiE "[_.a-z]+\.json" | perl -ne "chomp and print"`
-		printf "${TVC_version}, ${TVC_parameters},"
-	else
+		OUTPUT="${OUTPUT}${TVC_version}, ${TVC_parameters},"
+	#else
 #		printf ",,"
-		printf ""
+	#	printf ""
 	fi
 }
 
@@ -69,38 +71,49 @@ function check_TVC_Version {
 #Writes to output the result of finding and pushing the file
 function find_and_Push_File {
 	# this variable will only be set to True if the file was actually copied.
-	COPIED="False"
+	COPIED="True"
 	if [ "`find $1 -maxdepth 0 2>/dev/null`" ]; then
 		FILE_NAME=`find $1 -type f -printf "%f\n"`
-		if ssh ${USER_SERVER} stat ${OUT_PATH}/${FILE_NAME} \> /dev/null 2\>\&1; then
-			printf "File already copied,"
-#			echo "$1 already copied" 1>&2
+		if ssh ${USER_SERVER} stat ${DEST_PATH}/${FILE_NAME} \> /dev/null 2\>\&1; then
+			OUTPUT="${OUTPUT}File already copied,"
+#			echo "$1 already copied" >> $LOG
 		else
-			echo "${OUT_PATH}/${FILE_NAME} starting to copy at `date`" 1>&2
+			echo "${DEST_PATH}/${FILE_NAME} starting to copy at `date`" >> $LOG
 			COUNTER=0
 			until [ "$COUNTER" == "10" ]; do
-				rsync -avz --progress ${1} ${USER_SERVER}:${OUT_PATH}/${FILE_NAME} > /dev/null 2>&1 </dev/null
-				if [ "$?" -ne "0" ]; then
-					echo "Connection lost... will retry in 30 seconds." 1>&2
+				rsync -avz --progress ${1} ${USER_SERVER}:${DEST_PATH}/${FILE_NAME} > /dev/null 2>&1 </dev/null &
+				rsyncPID=$!
+				# sleepAndMaybeKill.sh is a bash command that sleeps for the given amount of time, then kills the ProcessID passed to it.
+				seconds_to_wait=7200
+				bash sleepAndMaybeKill.sh $seconds_to_wait $pushPID >/dev/null 2>&1 &  
+				SLEEP_AND_MAYBE_KILL_PID=$!   
+				wait $rsyncPID > /dev/null 2>&1
+				if [ $? -eq 0 ]; then
+					# If push_Data.sh succeeded, then stop the sleep and maybe kill script. 
+					kill $SLEEP_AND_MAYBE_KILL_PID >/dev/null 2>&1
+					COUNTER=10
+					COPIED="True"
+					OUTPUT="${OUTPUT}Copied,"
+					echo "${DEST_PATH}/${FILE_NAME} copied successfully `date`" >> $LOG
+				else
+					echo "Connection lost or rsync froze... will retry in 30 seconds." >> $LOG
 					let "COUNTER+=1"
 					sleep 30
 					#printf "ERROR: $1 was not copied,"
-				else
-					COUNTER=10
-					COPIED="True"
-					printf "Copied,"
-					echo "${OUT_PATH}/${FILE_NAME} copied successfully `date`" 1>&2
 				fi
 			done
 		fi
 	else
-		echo "--- File not found: $1 ---" 1>&2 
-	#	echo "${OUT_PATH}/${FILE_NAME}" >> files_not_found.txt
-		printf "not found," 
+		echo "--- File not found: $1 ---" >> $LOG 
+	#	echo "${DEST_PATH}/${FILE_NAME}" >> files_not_found.txt
+		OUTPUT="${OUTPUT}not found," 
 	fi
 }
 
 
+# default failenames
+LOG="push.log"
+OUTPUT_CSV="push_results.csv"
 
 #for arguments
 if [ $# -lt 14 ]; then # Technically there should be at least 14 arguments
@@ -115,7 +128,7 @@ do
 	let "counter+=1"
 	# If not enough inputs were given for an option, the while loop will just keep going. Stop it and print this error if it loops more than 100 times
 	if [ $counter -gt 100 ]; then
-		echo "USAGE ERROR: not all required inputs were given for options." 1>&2
+		echo "USAGE ERROR: not all required inputs were given for options."
 		#echo "$RUNNING"
 		exit 8
 	fi
@@ -128,14 +141,9 @@ do
 			RUNNING="$RUNNING --user_server: $2 "
 			shift 2
 			;;
-		-p | --project_path)
+		-p | --dest_path)
 			DEST_PATH=$2
-			RUNNING="$RUNNING --project_path: $2 "
-			shift 2
-			;;
-		-s | --sample)
-			SAMPLE=$2
-			RUNNING="$RUNNING --sample: $2 "
+			RUNNING="$RUNNING --dest_path: $2 "
 			shift 2
 			;;
 		-i | --run_id)
@@ -143,9 +151,14 @@ do
 			RUNNING="$RUNNING --run_id: $2 "
 			shift 2
 			;;
-		-r | --run)
-			RUN=$2
-			RUNNING="$RUNNING --output_dir: $2 "
+		-r | --run_json)
+			RUN_JSON=$2
+			RUNNING="$RUNNING --run_json: $2 "
+			shift 2
+			;;
+		-s | --sample_json)
+			SAMPLE_JSON=$2
+			RUNNING="$RUNNING --sample_json: $2 "
 			shift 2
 			;;
 		-P | --proton_name)
@@ -161,6 +174,16 @@ do
 		-b | --barcode) 
 			BARCODE=$2
 			RUNNING="$RUNNING --barcode: $2 "
+			shift 2
+			;;
+		-o | --output_csv) 
+			OUTPUT_CSV=$2
+			RUNNING="$RUNNING --output_csv: $2 "
+			shift 2
+			;;
+		-l | --log_file) 
+			LOG=$2
+			RUNNING="$RUNNING --log_file: $2 "
 			shift 2
 			;;
 		-*)
@@ -179,14 +202,22 @@ do
 done
 
 # the options used
-echo "$RUNNING at `date`" 1>&2
+echo "$RUNNING at `date`" >> $LOG
+
+#first, check the connection of the server.
+# this check would be too slow.
+#ssh -q ${1} exit
+#if [ "$?" != "0" ]; then
+#	echo "ERROR: Cannot connect to ${USER_SERVER}" 
+#	exit 1
+#fi
 
 #-----------------------------  BEGIN PUSHING HERE -----------------------------------
 #--------------------------------------------------------------------------------------
 
 # The path to push the files to
 # Example /rawdata/projects/Einstein/E0001/Run1
-OUT_PATH=${DEST_PATH}/${SAMPLE}/${RUN}
+#OUT_PATH=${DEST_PATH}/${SAMPLE}/${RUN}
 PROJECT_PATH="/results/analysis/output/Home"
 
 #Find the path of the directory that has the specified ROT-ID
@@ -200,42 +231,46 @@ elif [ "`find ${BACKUP_PATH}/*${PROTON_NAME}-${RUN_ID}[-_]*/${BARCODE}*rawlib*.b
 	RUN_PATH=`find ${BACKUP_PATH}/*${PROTON_NAME}-${RUN_ID}[-_]* -maxdepth 0 -type d | head -n 1` 
 else
 	#That rotID was not found, so print error, and quit.
-	echo "*${PROTON_NAME}-${RUN_ID}-*/${BARCODE}*rawlib.bam not found in ${PROJECT_PATH} or ${BACKUP_PATH}." 1>&2
+	echo "*${PROTON_NAME}-${RUN_ID}-*/${BARCODE}*rawlib.bam not found in ${PROJECT_PATH} or ${BACKUP_PATH}." >> $LOG
 #	echo "${RUN_ID}/${RUN_NUM}_rawlib.bam" >> files_not_found.txt
-	printf "*${PROTON_NAME}-${RUN_ID}-*/${BARCODE}*rawlib.bam not found in ${PROJECT_PATH} or ${BACKUP_PATH}."
+	echo "*${PROTON_NAME}-${RUN_ID}-*/${BARCODE}*rawlib.bam not found in ${PROJECT_PATH} or ${BACKUP_PATH}." >> $OUTPUT_CSV
 	exit 1
 fi
 
-printf "$RUN_PATH,"
+#OUTPUT is the variable used to keep track of all of the files copied. Is printed out at the end
+OUTPUT="$RUN_PATH,"
 
 # Begin the file transfering!
 # Finds the TS and TVC versions that were used to compare for QC later.
 check_TS_Version "${RUN_PATH}/version.txt"
 check_TVC_Version "${RUN_PATH}/plugin_out/variantCaller_out/drmaa_stdout.txt"
 # Create the Sample and run path
-create_ssh_Dir "${OUT_PATH}"
+create_ssh_Dir "${DEST_PATH}"
 
 #push the BAM file
-# if the run is not barcoded, then barcode will be empty
+# if the run is not barcoded, then barcode variable will be empty, so rawlib.bam will be pushed.
 find_and_Push_File "${RUN_PATH}/${BARCODE}*rawlib.bam"
 if [ "$COPIED" == "True" ]; then
 	if [ "$TS_version" == "" ]; then
 		TS_version="not_found"
 	fi
-	# Write the json file which will be used to run TVC, COV analysis, and QC the runs.
-	# FILE_NAME should be the name of the file that was pushed
-	python writeJson.py \
-		--bam $FILE_NAME \
-		--run_name $RUN \
-		--sample $SAMPLE \
-		--proj_path $DEST_PATH \
-		--orig_path $RUN_PATH \
-		--proton $PROTON_NAME \
-		--ip_address 192.168.200.42 \
-		--ts_version $TS_version \
-		--json "Json_Files/${SAMPLE}_${RUN}.json"
+	python addToJson.py --json $RUN_JSON \
+		--metric '{"orig_path":"'"$RUN_PATH"'"}' \
+		--metric '{"analysis":{"files":["'"$FILE_NAME"'"]}' \
+		--metric '{"ts_version":"'"$TS_version"'"}'
+		#--metric '{"torrent_suite_link":"http://'"TS_version"'"}'
 	# push the Json File
-	find_and_Push_File "Json_Files/${SAMPLE}_${RUN}.json"
+	find_and_Push_File "${RUN_JSON}"
+
+	# if the sample's json file has already been written, add a run to it and recopy it over.
+	# checking to see if a file exists over ssh in python is not easy, so I did it here.
+	if ssh ${USER_SERVER} stat ${SAMPLE_JSON} \> /dev/null 2\>\&1; then
+		# the run's json file has the path to it's sample's json file. It will copy the sample's json file from the other server and add the run to it.
+		python addToJson.py --json "$RUN_JSON" --add_run_to_sample --server "$USER_SERVER"
+	# else push a new sample json file through addToJson.
+	else
+		python addToJson.py --json "$RUN_JSON" --push_sample_json --server "$USER_SERVER"
+	fi
 fi
 
 #push the BAM.bai file
@@ -245,23 +280,19 @@ find_and_Push_File "${RUN_PATH}/${BARCODE}*rawlib.bam.bai"
 # starting with 4.2 coverage analysis plugin output folder gets a number assigned so taht users can run multiple times without over-writing the previous coverage analysis plugin result 
 # example: coverageAnalysis_out.2369
 # if there are multiple cov results, hopefully they were run with the same BED file! find will grep the first instance
-covPath=`find ${RUN_PATH}/plugin_out/coverageAnalysis_out.* -maxdepth 0 2>/dev/null`
+covPath=`find ${RUN_PATH}/plugin_out/coverageAnalysis_out* -maxdepth 0 2>/dev/null | head -n 1`
 find_and_Push_File "${covPath}/*.amplicon.cov.xls"
-
-# find_and_Push_File "${RUN_PATH}/plugin_out/coverageAnalysis_out/*.amplicon.cov.xls"
 
 #push the VCF file
 find_and_Push_File "${RUN_PATH}/plugin_out/variantCaller_out/TSVC_variants.vcf"
-# push the backupPDF. It is located in the non-archived dir.
-#if [ "`find ${PROJECT_PATH}/*${PROTON_NAME}-${RUN_ID}-*/backupPDF.pdf -type f 2>/dev/null`" ]; then
-#	pdf=`find ${PROJECT_PATH}/*${PROTON_NAME}-${RUN_ID}-*/backupPDF.pdf -type f 2>/dev/null | head -n 1`
-#	find_and_Push_File "$pdf"
-#fi
 
-#let's copy report.pdf instead of backup  
-if [ "`find ${PROJECT_PATH}/*${PROTON_NAME}-${RUN_ID}-*/report.pdf -type f 2>/dev/null`" ]; then
-	pdf=`find ${PROJECT_PATH}/*${PROTON_NAME}-${RUN_ID}-*/report.pdf -type f 2>/dev/null | head -n 1`
+# push the reportPDF. It is located in the non-archived dir.
+if [ "`find ${PROJECT_PATH}/*${PROTON_NAME}-${RUN_ID}[-_]*/report.pdf -type f 2>/dev/null`" ]; then
+	pdf=`find ${PROJECT_PATH}/*${PROTON_NAME}-${RUN_ID}[-_]*/report.pdf -type f 2>/dev/null | head -n 1`
 	find_and_Push_File "$pdf"
 fi
+
+# write the results of copying each file to the output CSV.
+echo "$OUTPUT" >> $OUTPUT_CSV
 
 #That should be about it for now.
---
+++ Push/start_Push.py
@@ -0,0 +1,202 @@
+#! /usr/bin/env python
+
+import os
+import os.path
+import sys
+import re
+from optparse import OptionParser
+import json
+
+
+# some global variables
+PLUTO_BACKUP_PATH="/mnt/Charon/archivedReports"
+MERCURY_BACKUP_PATH="/mnt/Triton/archivedReports"
+PGM_BACKUP_PATH="/media/Backup_02/archivedReports"
+
+class Push_Data:
+	def __init__(self, options):
+		self.options = options
+		try:
+			self.ex_json = json.load(open(options.ex_json))
+		except ValueError:
+			print "ERROR: %s is not formatted correctly"%self.options.ex_json
+			sys.exit(1)
+		#self.ex_json = {'sample_type': 'germline'}
+		#self.ex_json = {'sample_type': 'tumor_normal', 'project': 'testing'}
+
+		if self.options.proton == "PLU":
+			self.backup_path = PLUTO_BACKUP_PATH
+			self.ip = "192.168.200.42"
+		elif self.options.proton == "MER" or self.options.proton == "NEP":
+			self.backup_path = MERCURY_BACKUP_PATH
+			self.ip = "192.168.200.41"
+		elif self.options.proton == "ROT":
+			self.backup_path = PGM_BACKUP_PATH
+			self.ip = "130.132.19.237"
+	
+	# this function is meant to ensure the right columns are used to gather the run metadata when pushing a run.
+	# @param header_line the header line of the CSV to push. not yet implemented
+	def find_header_indexes(self, header_line):
+		if self.ex_json['sample_type'] == 'tumor_normal':
+			self.headers = {'sample': 0, 'tumor_normal': 1, 'run_id': 2, 'run_num': 3, 'barcode': 4, 'proton': 5}
+		else:
+			self.headers = {'sample': 0, 'run_id': 1, 'run_num': 2, 'barcode': 3, 'proton': 4}
+		# store the item at self.headers["proton"] as either PLU, NEP, MER, or ROT
+
+	# @param run the line of the CSV 
+	def push_run(self, run):
+		#print "pushing run: ", run.strip()
+		run = run.strip().split(",")
+		#print run
+		#print self.headers['run_num']
+		# first get the proton and run_num
+		if len(run[self.headers["run_num"]].split('-')) > 1:
+			proton = run[self.headers["run_num"]].split('-')[0]
+			run_num = run[self.headers["run_num"]].split('-')[1]
+		else:
+			proton = run[self.headers["proton"]]
+			run_num = run[self.headers["run_num"]]
+
+		run_path = ''
+		if self.ex_json['sample_type'] == 'tumor_normal':
+			if run[self.headers['tumor_normal']] == "N":
+				run_path = "%s/%s/Normal/N-%s" %(self.options.destination, run[self.headers["sample"]], run_num)
+				run_name = "N-" + run_num
+				run_type = "normal"
+			elif run[self.headers['tumor_normal']] == "T":
+				run_path = "%s/%s/Tumor/T-%s" %(self.options.destination, run[self.headers["sample"]], run_num)
+				run_name = "T-" + run_num
+				run_type = "tumor"
+		else:
+			run_path = "%s/%s/Run%s"%(self.options.destination, run[self.headers["sample"]], run_num)
+			run_name = "Run" + run_num
+			run_type = "normal"
+		# check if the proton names or pgm names match. They should be None if this is not a proton
+		if self.options.proton == proton:
+			# first write the new run's json file
+			run_json = self.write_run_json(run_num, run_name, run_type, run[self.headers['sample']], run_path)
+			# first write the sample's json file
+			sample_json = self.write_sample_json(run[self.headers['sample']], "%s/%s_%s.json"%(run_path, run[self.headers['sample']], run_name))
+		
+			# submit the push_Data script to SGE to copy the sample.
+			push_command = "qsub -N Push_%s push_Data.sh "%run[self.headers['sample']] + \
+				 "--user_server %s "%self.options.server + \
+				 "--dest_path %s "%run_path + \
+				 "--run_id  %s "%run[self.headers['run_id']] + \
+				 "--run_json  %s "%run_json + \
+				 "--sample_json  %s "%sample_json + \
+				 "--proton_name %s "%proton + \
+				 "--output_csv %s "%self.options.output_csv + \
+				 "--backup_path %s "%self.backup_path 
+			if re.search("Ion", run[self.headers['barcode']]):
+				 push_command += " --barcode %s "%run[self.headers['barcode']]
+			status = self.runCommandLine(push_command)
+
+	def write_run_json(self, runNum, runName, runType, sample, run_path):
+		json_name = "%s_%s.json"%(sample,runName)
+		# Write the run's json file which will be used mainly to hold metrics.
+		jsonData = {
+			"json_file": "%s/%s"%(run_path,json_name), 
+			"run_folder": run_path, 
+			"run_name": runName, 
+			"run_num": runNum, 
+			"run_type": runType, 
+			"pass_fail_status": "pass", 
+			"project": self.ex_json['project'], 
+			"proton": self.options.proton,
+			"sample": sample, 
+			"sample_folder": "%s/%s"%(self.options.destination, sample),
+			"sample_json": "%s/%s/%s.json"%(self.options.destination, sample, sample),
+		}
+
+		# make sure hte JsonFiles directory exists
+		if not os.path.isdir("Json_Files"):
+			os.mkdir("Json_Files")
+
+		# dump the json file
+		with open("Json_Files/"+json_name, 'w') as out:
+			json.dump(jsonData, out, sort_keys=True, indent = 2)
+
+		# the path of the run's json file to push
+		return "Json_Files/"+json_name
+
+
+	# @param sample the name of the current sample
+	# @param run_path the path of the current run
+	def write_sample_json(self, sample, run_json):
+		sample_path = "%s/%s"%(self.options.destination, sample)
+
+		# edit the sample's json file with this sample's info. The other metrics in the sample JSON file should already be set. 
+		self.ex_json["json_file"] = "%s/%s.json"%(sample_path, sample) 
+		self.ex_json["output_folder"] = "%s/QC"%sample_path 
+		self.ex_json["runs"] = [run_json]
+		self.ex_json["sample_name"] = sample
+		self.ex_json["sample_folder"] = sample_path
+
+		# dump the json file
+		with open("Json_Files/%s.json"%sample, 'w') as out:
+			json.dump(self.ex_json, out, sort_keys=True, indent = 2)
+
+		# this path will be used to check if the sample's json exists on the server already
+		return "%s/%s.json"%(sample_path, sample)
+
+	def runCommandLine(self, systemCall):
+		#run the call and return the status
+		print 'Starting %s' % (systemCall)
+		status = os.system(systemCall)
+		return(status)
+
+
+if __name__ == '__main__':
+
+	# set up the option parser
+	parser = OptionParser()
+	
+	# add the options to parse
+	parser.add_option('-s', '--server', dest='server', help='The server to push data to. <ionadmin@ipaddress>')
+	parser.add_option('-d', '--destination', dest='destination', help='The destination path where the sample will be copied to.')
+	parser.add_option('-i', '--input', dest='input', help='The input csv file containing the metadata about each sample to be pushed.')
+	parser.add_option('-o', '--output_csv', dest='output_csv', default='Push_Results.csv', help='The results of copying will be placed in this file. Default: [%default]')
+	parser.add_option('-l', '--log', dest='log', default="Push.log", help='Default: [%default]')
+	#parser.add_option('-t', '--tumor_normal', dest='tn', action="store_true", help='If the project for which samples are being copied is a Tumor/Normal comparison project, use this option. \
+	#		Otherwise file structure will be treated as a germline only study.')
+	parser.add_option('-j', '--example_json', dest='ex_json', help='The example json file containing the settings necessary for this project. Should be different for every project. For help of how to create the example json file, see the protocls')
+	parser.add_option('-p', '--proton', dest='proton', help='The name of the proton or pgm from which you are pushing the files. Options: "PLU", "MER", "NEP, "ROT"')
+
+
+	(options, args) = parser.parse_args()
+
+	# check to make sure the inputs are valid
+	if not options.input or not options.ex_json:
+		print "--USAGE-ERROR-- --input and --example_json required"
+		parser.print_help()
+		sys.exit(1)
+	if not os.path.isfile(options.input) or not os.path.isfile(options.ex_json):
+		print "--USAGE-ERROR-- %s or %s not found"%options.input, options.ex_json
+		parser.print_help()
+		sys.exit(1)
+
+	# check if the proton name is valid
+	valid_protons = ['PLU', 'MER', 'NEP', 'ROT']
+	if options.proton not in valid_protons:
+		print "--USAGE-ERROR-- %s not a valid proton"%options.proton
+		parser.print_help()
+		sys.exit(1)
+
+	# check if the IP address is valid. More IP addresses can be added overtime
+	# ips: pluto, mercury, triton, triton's external, lam, bonnie's pgm
+	valid_ips = ["ionadmin@192.168.200.42", "ionadmin@192.168.200.41", "ionadmin@192.168.200.131", "ionadmin@12.32.211.40", "ionadmin@192.168.200.214", "ionadmin@130.132.19.237"] 
+	if options.server not in valid_ips:
+		print "--USAGE-ERROR-- %s not a valid server or ipaddress to push to. If it is, sorry for being overly stringent. You can add the ip address you're trying to add to the list"%options.server
+		parser.print_help()
+		sys.exit(1)
+
+	pusher = Push_Data(options)
+	with open(options.input, 'r') as input_file:
+		header_line = input_file.readline().strip()
+		pusher.find_header_indexes(header_line)
+		# push each run in the file
+		for run in input_file:
+			pusher.push_run(run)
+
+	print "Finished submitting runs to be pushed"
