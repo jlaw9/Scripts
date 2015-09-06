27eb17efa0985c2208835b4efe4a9483dcc48a19
Matt <matt@Ozlems-MacBook-Pro.local>
2015-09-05 20:34:57 -0600

    update

--- variantmanager/create_3x3.py
+++ variantmanager/create_3x3.py
@@ -1,37 +1,27 @@
 __author__ = 'matt'
 import argparse
 
+class Create3x3:
+    def __init__(self, normal_vcf, tumor_vcf, run_depths):
+        self.normal_vcf = normal_vcf
+        self.tumor_vcf = tumor_vcf
+        self.run_depths = run_depths
 
+    def run(self):
+        print self.normal_vcf
+        print self.tumor_vcf
+        print self.run_depths
 
 if __name__ == '__main__':
 
-    # setup the option parser
     parser = argparse.ArgumentParser(description='VariantManager is a software suite that provides '
                                                  'several variant managing services.')
 
-    # add universal arguments, arguments to be specified regardless of the type of arguments that follow.
-    parser.add_argument('project_name', help='The name of the project being used.')
-    parser.add_argument('-cpu', '--cpu_number', metavar='cpu_number', required=False,
-                        help='The number of processes to run the executions on.', default=10)
+    parser.add_argument('-n','--normal_vcf', help='', required=True)
+    parser.add_argument('-t','--tumor_vcf', help='', required=True)
+    parser.add_argument('-rd','--run_depths', help='', required=True)
 
-    # add the subparsers for various arguments
-    subparsers = parser.add_subparsers(help="The argument specifying the type of analysis to perform." +
-                                            " example: ./variant_manager.py all")
+    args = parser.parse_args()
 
-    # Create the Different Subparsers
-    parser_setup = subparsers.add_parser('setup', help='Setup the project with a config file.')
-    parser_add = subparsers.add_parser('add', help='add a new sample to the database')
-    parser_delete = subparsers.add_parser('delete', help="Delete information from the database")
-    parser_load = subparsers.add_parser('load', help='Load variants from vcf files into database and generate hotspot'
-                                                     'list')
-    parser_stats = subparsers.add_parser('stats', help='Perform a statistical analysis of the data')
-    parser_hotspot = subparsers.add_parser('hotspot', help='Perform a hotspot analysis using TVC')
-    parser_output = subparsers.add_parser('output', help='Output information of interest')
-    parser_nothing = subparsers.add_parser('nothing')
-
-    # Specifying options for the parser_setup
-    parser_setup.set_defaults(which="setup")
-    parser_setup.add_argument('-c', '--config', help='The path to the configuration file to be used by the project, '
-                                                     'see readme for more information.', required=True)
-    # Specify the options for the parser_add
-    parser_add.set_defaults(which="add")
+    creator = Create3x3(args.normal_vcf, args.tumor_vcf, args.run_depths)
+    creator.run()
