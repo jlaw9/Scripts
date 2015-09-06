cd389fa2c784b704274184a155c00c5b10a9daf3
Matt <matt@Ozlems-MacBook-Pro.local>
2015-09-05 21:21:04 -0600

    update

--- variantmanager/create_3x3.py
+++ variantmanager/create_3x3.py
@@ -30,7 +30,7 @@ class Create3x3:
     def get_change_counts(self):
         self.change_counts = {'WT_WT':0, 'WT_HET':0, "WT_HOM":0,'HET_WT':0, 'HET_HET':0, "HET_HOM":0, \
                 'HOM_WT':0, 'HOM_HET':0, "HOM_HOM":0}
-        for line in self.matched_variants:
+        for line in open(self.matched_variants, 'r'):
             line = line.strip().split()
             print line
 
