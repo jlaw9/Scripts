0185db9e9c8acf8f44cc4b5a2b4fad899e45babe
Matt <matt@Ozlems-MacBook-Pro.local>
2015-09-16 14:44:07 -0600

    update

--- Other/create_master_3x3.py
+++ Other/create_master_3x3.py
@@ -12,4 +12,20 @@ for root, dirnames, filenames in os.walk(sys.argv[1]):
 for match in matches:
     if "QC/results" in match:
         results_file = json.load(open(match,'r'))
-        print match, results_file['QC_comparisons'].keys()
+
+        three_by_three = results_file['QC_comparisons']['all']
+        print match.split("/")[-2]
+        print "error rate: %s" % three_by_three['error_rate']
+        print "% available bases: %s" % three_by_three['perc_avail_bases']
+        print "# of GTs reassigned: %s" % three_by_three['reassigned_GTs']
+        print "\tWT\tHET\tHOM\tSum:"
+        print "WT\t%d\t%d\t%d\t%d" % (three_by_three['WT_WT'], three_by_three['WT_HET'], three_by_three['WT_HOM'],
+                                     sum([three_by_three['WT_WT'], three_by_three['WT_HET'], three_by_three['WT_HOM']]))
+        print "HET\t%d\t%d\t%d\t%d" % (three_by_three['HET_WT'], three_by_three['HET_HET'], three_by_three['HET_HOM'],
+                                     sum([three_by_three['HET_WT'], three_by_three['HET_HET'], three_by_three['HET_HOM']]))
+        print "HOM\t%d\t%d\t%d\t%d" % (three_by_three['HOM_WT'], three_by_three['HOM_HET'], three_by_three['HOM_HOM'],
+                                     sum([three_by_three['HOM_WT'], three_by_three['HOM_HET'], three_by_three['HOM_HOM']]))
+        print "Sum:\t%d\t%d\t%d\t%d" % (sum([three_by_three['WT_WT'], three_by_three['HET_WT'], three_by_three['HOM_WT']]),
+                                        sum([three_by_three['WT_HET'], three_by_three['HET_HET'], three_by_three['HOM_HET']]),
+                                        sum([three_by_three['WT_HOM'], three_by_three['HET_HOM'], three_by_three['HOM_HOM']]),
+                                        three_by_three['total_eligible_bases'])
