603774f301bd889c8db6765d85e1f3d75fa44875
Matt <matt@Ozlems-MacBook-Pro.local>
2015-10-19 19:26:37 -0600

    update

--- variantmanager/varman2/output.py
+++ variantmanager/varman2/output.py
@@ -121,13 +121,13 @@ class Output:
         csv_writer = csv.writer(open(out_path, "w"), delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
 
         header = ['CHROM', 'POS', 'REF', 'ALT', 'GT', 'FREQ', 'READ_DEPTH', 'QC_Final',
-                  'QC_Cov', 'QC_AF', 'QC_MULTI_ALLELE']
+                  'QC_Cov', 'QC_AF', 'QC_MULTI_ALLELE', 'TOST_P','TTEST_P']
         csv_writer.writerow(header)
 
         for variant in variants_mongo.get_all_vars('orig'):
             var_info = [variant['CHROM'], variant['POS'], variant['REF'], variant['ALT'], variant['GT_calc'],
                         variant['AF_calc'], variant['READ_DEPTH'], variant['FINAL_QC'], variant['COV_QC'],
-                        variant['AF_QC']]
+                        variant['AF_QC'], variant['TTEST_P'], variant['TOST_P']]
             print "\t".join([str(val) for val in var_info])
 
         return out_path
